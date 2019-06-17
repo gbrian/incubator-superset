ed0d155955e4d956eb4e353ac4ffa0f918104103
gbrian <gustavo.brian@gmail.com>
2019-06-17 14:44:37 +0200

    Add b64thumbnail column to dashboard

--- superset/models/core.py
+++ superset/models/core.py
@@ -413,6 +413,8 @@ class Dashboard(Model, AuditMixinNullable, ImportMixin):
     export_fields = ('dashboard_title', 'position_json', 'json_metadata',
                      'description', 'css', 'slug')
 
+    b64thumbnail = Column(Text)
+
     def __repr__(self):
         return self.dashboard_title or str(self.id)
 
